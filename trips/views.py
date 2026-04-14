e6519ec2c796a2e78f2e69d2508d3ad84239a6d5
IcoPv_PC <162989048+IcoPv@users.noreply.github.com>
2026-04-14 23:41:11 +0300

    feat: add trip htmls refactor: minor bug fixes

--- trips/views.py
+++ trips/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Q
 from django.contrib import messages
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
